18507cd4cc9b38173602576c7e5bc91026ebbada
セルゲイ・シンカレンコ <exp33int@gmail.com>
2024-10-08 01:07:06 +0500

    feat: implement funcs

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -130,8 +130,9 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const str = String(value);
+  return Number(str[str.length - 1]);
 }
 
 
@@ -146,8 +147,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
