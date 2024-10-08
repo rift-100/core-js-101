03eea5d2bf5dfab049073100f12f5a0d5ba3c84b
セルゲイ・シンカレンコ <exp33int@gmail.com>
2024-10-08 18:56:48 +0500

    feat: implement func

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -186,8 +186,12 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  if (pow === 0) {
+    return num;
+  }
+  const res = Math.round(num / (10 ** pow)) * (10 ** pow);
+  return res;
 }
 
 /**
