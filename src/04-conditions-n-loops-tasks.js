487efa977d592be5cb83e03ff5dbf5b7ba4f4697
セルゲイ・シンカレンコ <exp33int@gmail.com>
2024-10-17 17:48:32 +0500

    feat: implement doRectanglesOverlap func

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -141,8 +141,13 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  if (rect1.top + rect1.height >= rect2.top
+    && rect1.left + rect1.width >= rect2.left
+  ) {
+    return true;
+  }
+  return false;
 }
 
 
