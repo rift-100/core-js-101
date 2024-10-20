f895c1a0efcd03823eaedcc8654ffced857683c6
セルゲイ・シンカレンコ <exp33int@gmail.com>
2024-10-20 14:37:55 +0500

    feat: implement getIntervalString func

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -226,8 +226,14 @@ function findFirstSingleChar(str) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  let res = '';
+  res += isStartIncluded ? '[' : '(';
+  res += `${min}, ${max}`;
+  res += isEndIncluded ? ']' : ')';
+  return res;
 }
 
 
