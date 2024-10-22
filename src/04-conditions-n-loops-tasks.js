e5227675a539e63dfbb11579d47122e963a17027
セルゲイ・シンカレンコ <exp33int@gmail.com>
2024-10-23 00:23:27 +0500

    feat: implement getDigitalRoot func

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -329,8 +329,13 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let sum = 0;
+  if (num < 10) {
+    return num;
+  }
+  sum += getDigitalRoot(Number(String(num).split('').reduce((acc, n) => acc + Number(n), 0)));
+  return sum;
 }
 
 
